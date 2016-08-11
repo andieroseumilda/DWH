ae7c0e6e5fdfdda2e415c5923b20c5a371c071fe
andierose.umilda@gmail.com <andierose.umilda@gmail.com>
2016-08-11 14:02:37 +0800

    updatePage2

--- Webdriver/src/InternetBookingEngine/InputDataIBE.java
+++ Webdriver/src/InternetBookingEngine/InputDataIBE.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 public class InputDataIBE {
 	private GetAllPages dwhIbe;
 	
-	public InputDataIBE() throws MalformedURLException {
+	public InputDataIBE() throws MalformedURLException{
 		dwhIbe = new GetAllPages();
 	}
 	
 	@Test
 	public void bookingEngine_DWH() throws MalformedURLException {
-		dwhIbe.makeReservation("DWH", 2);
+		dwhIbe.makeReservation("staging", "DWH", 2);
 	}
 
 //	@Test
