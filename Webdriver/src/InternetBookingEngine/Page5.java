354bf26147e32c6345a70ebe52d6db75a25b77c2
andierose.umilda@gmail.com <andierose.umilda@gmail.com>
2016-10-07 10:33:53 +0800

    generate csv file

--- Webdriver/src/InternetBookingEngine/Page5.java
+++ Webdriver/src/InternetBookingEngine/Page5.java
@@ -1,7 +1,10 @@
 package InternetBookingEngine;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -9,72 +12,78 @@ import java.util.*;
 import org.openqa.selenium.WebDriver;
 
 public class Page5 {
-
+	private String filePath = "E:\\1\\eclipse-jee-luna-SR2-win32\\eclipse\\EclipseCsfFile\\Test_Reservation.csv";
 	private locator_step5 step5;
 	private String intro;
 	private String disclaimer;
-
 	private FileWriter writer;
 	private Page5_Labels labels;
 	private Page5_Values values;
-	private Page5_Reservation_details get_csvFileName;
-	private ArrayList<HashMap<Integer, String>> myListv1;
-	private ArrayList<HashMap<Integer, String>> myListv2;
-	private File fileExist;
+	private ArrayList<HashMap<Integer, String>> get_label_list;
+	private ArrayList<HashMap<Integer, String>> get_values_list;
+	private File createFile;
 	
+
 	public Page5(WebDriver driver) {
 		step5 = new locator_step5(driver);
 		labels = new Page5_Labels(driver);
 		values = new Page5_Values(driver);
-		get_csvFileName = new Page5_Reservation_details();
 	}
 
 	public void confirmPage(String payment_settings, String room_name, boolean cc_owner) throws IOException {
-		myListv1 = labels.get_confirmPage_labels();
-		fileExist = new File(get_csvFileName.setCsvFile());
-		writer = null;	
-		
 		intro = step5.introCpage().getText();
 		System.out.println(intro);
 		disclaimer = step5.label_disclaimer().getText();
 		System.out.println(disclaimer);
-		/*
-		 * Conditions		
-		 */
 
-		try{	
-			if(!fileExist.exists()){
-				writer = new FileWriter(get_csvFileName.setCsvFile(),true);
-				for(int cell_row = 0; cell_row < myListv1.size(); cell_row++){
-					for(int cell_column = 0; cell_column < 20; cell_column++){
-						writer.append(myListv1.get(cell_row).get(cell_column).toString().concat(","));	
-					}
-					writer.append("\r\n");
-				}
-				loopValues(payment_settings, room_name, cc_owner);
-
-			} else {
-				writer = new FileWriter(get_csvFileName.setCsvFile(), true);
-				loopValues(payment_settings, room_name, cc_owner);
+		// Create and Write Reservation Details to CSV
+		createFile = new File(filePath);		
+		try {
+			writer = new FileWriter(filePath, true);
+			if(!file_has_reservationDetails_label()){
+				get_label_list = labels.get_confirmPage_labels(); 
+				write_csv_reservationDetails(get_label_list);
 			}
-
+			get_values_list = values.get_confirmPage_values(payment_settings, room_name, cc_owner);
+			write_csv_reservationDetails(get_values_list);
+			
 		} catch(Exception e){
 			e.printStackTrace();
 		}
 
+		// Close CSV
 		System.out.println(disclaimer);
 		writer.flush();
 		writer.close();
 	}
 	
-	private void loopValues(String payment_settings, String room_name, boolean cc_owner) throws IOException{
-		myListv2 = values.get_confirmPage_values(payment_settings, room_name, cc_owner);
-		for(int cell_row = 0; cell_row < myListv2.size(); cell_row++){
+	private void write_csv_reservationDetails(ArrayList<HashMap<Integer, String>> data) throws IOException {
+		for(int cell_row = 0; cell_row < data.size(); cell_row++){
 			for(int cell_column = 0; cell_column < 20; cell_column++){
-				writer.append("\"");
-				writer.append(myListv2.get(cell_row).get(cell_column).toString().concat("\","));	
+				add_backlash_character(get_values_list);
+				writer.append(data.get(cell_row).get(cell_column).toString());
+				add_backlash_character(get_values_list);
+				writer.append(",");
 			}
 			writer.write("\r\n");
+		}		
+	}
+	
+	private void add_backlash_character(ArrayList<HashMap<Integer, String>> data) throws IOException {
+		if (get_values_list != null) {
+			if (data.equals(get_values_list)) {
+				writer.append("\"");					
+			}			
+		}
+	}
+	
+	private boolean file_has_reservationDetails_label() throws IOException {
+		@SuppressWarnings("resource")
+		BufferedReader reader = new BufferedReader(new FileReader(createFile));
+		if (reader.readLine() != null) {
+		    return true;
+		} else {
+			return false;
 		}
 	}
 
